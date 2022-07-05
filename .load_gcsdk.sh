9417f692af323405632d1fe31dce02ab61321737
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2022-07-05 07:30:27 +0200

    Delete .load_gcsdk.sh

--- .load_gcsdk.sh
+++
@@ -1,3 +0,0 @@
-export GCSDK_DIR="$HOME/.google-cloud-sdk"
-if [ -f "$GCSDK_DIR/path.zsh.inc" ]; then source "$GCSDK_DIR/path.zsh.inc"; fi
-if [ -f "$GCSDK_DIR/completion.zsh.inc" ]; then source "$GCSDK_DIR/completion.zsh.inc"; fi
