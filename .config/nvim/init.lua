447fe3616ed1a7a87a2ce66e787957f011195e0f
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-03-20 14:29:04 +0100

    Add nvim-telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,5 +19,16 @@ require("lazy").setup({
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
 })
 
+-- https://github.com/nvim-telescope/telescope.nvim
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
