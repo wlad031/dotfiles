23421ae8b3d70eab0b13e06bfe085174667286b4
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-03-21 21:49:39 +0100

    [autocommit] 2024-03-21T21:49:39+0100

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,9 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- lazy.nvim installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -15,16 +21,20 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
-require("lazy").setup({
+local plugins = {
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
-})
+  { "nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { 'nvim-lua/plenary.nvim' } },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
+}
+
+local opts = {
+
+}
+
+require("lazy").setup({ plugins, opts })
 
 -- https://github.com/nvim-telescope/telescope.nvim
 local builtin = require('telescope.builtin')
@@ -32,3 +42,20 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local telescope = require('telescope')
+
+telescope.setup {
+	pickers = {
+    find_files = { hidden = true }
+	}
+}
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "java", "python", "bash", "markdown", "xml", "json", "yaml"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
