853408e4bb918aa82a943c1a37718e29d6dcff4f
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-03-26 22:11:42 +0100

    prettify noice nvim

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,40 +1,60 @@
-local _ = {
+return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-  },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
   },
-  setup = function()
-    require("noice").setup({
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+        border = {
+          style = "none",
+          padding = { 2, 3 },
+        },
+        filter_options = {},
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
         },
       },
-      -- you can enable a preset for easier configuration
-      --      presets = {
-      --      bottom_search = true,         -- use a classic bottom cmdline for search
-      --command_palette = true,       -- position the cmdline and popupmenu together
-      --    long_message_to_split = true, -- long messages will be sent to a split
-      --  inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-      --lsp_doc_border = false,       -- add a border to hover docs and signature help
-      --},
-      cmdline = {
-        view = "cmdline",
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
       },
-    })
+    },
+    cmdline = {
+      format = {
+        search_down = {
+          view = "cmdline",
+        },
+        search_up = {
+          view = "cmdline",
+        },
+      },
+    },
+  },
+  setup = function()
+    require("noice").setup({})
   end
-}
-return {
-  { "rcarriga/nvim-notify" }
 }
