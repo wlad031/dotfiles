c83ec48f8700f4bfde303917858ba2c1943665f3
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-03-27 17:17:20 +0100

    vim test

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -225,5 +225,15 @@ return {
         group = nvim_metals_group,
       })
     end
+  },
+  {
+    "vim-test/vim-test",
+    dependencies = {
+      "preservim/vimux",
+    },
+    vim.keymap.set('n', '<leader>tf', ':TestFile<CR>'),
+    vim.keymap.set('n', '<leader>tt', ':TestNearest<CR>'),
+    vim.keymap.set('n', '<leader>tl', ':TestLast<CR>'),
+    vim.cmd("let test#strategy = 'vimux'"),
   }
 }
