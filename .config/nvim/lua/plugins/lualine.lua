97ed336c4fd7bb61e2e9e5784abe56f30a74f117
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-03-27 12:08:25 +0100

    nice working lualine with copilot status

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,46 +1,53 @@
+function copilot()
+  return {
+    'copilot',
+    symbols = {
+      status = {
+        hl = {
+          enabled = "#50FA7B",
+          sleep = "#AEB7D0",
+          disabled = "#6272A4",
+          warning = "#FFB86C",
+          unknown = "#FF5555"
+        }
+      },
+      spinners = require("copilot-lualine.spinners").dots,
+      spinner_color = "#6272A4"
+    },
+    show_colors = true,
+    show_loading = true
+  }
+end
+
 return {
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  },
-  {
-    'AndreM222/copilot-lualine',
-    setup = function()
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = false },
+    config = function()
       local lualine = require("lualine")
       lualine.setup({
         options = {
-            icons_enabled = true,
-            theme = auto,
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
-            disabled_filetypes = {},
-            always_divide_middle = true
+          theme = 'auto',
+          icons_enabled = true,
         },
         sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff',
-                {
-                    'diagnostics',
-                    sources = { "nvim_diagnostic" },
-                    symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
-                }
-            },
-            lualine_c = { 'filename' },
-            lualine_x = { 'copilot' ,'encoding', 'fileformat', 'filetype' }, -- I added copilot here
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch' },
+          lualine_c = { 'filename' },
+          lualine_x = { copilot(), 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
         },
         inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
-            lualine_y = {},
-            lualine_z = {}
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
         },
-        tabline = {},
-        extensions = {}
-    })
-    end
-  }
+      })
+    end,
+  },
+  { 'AndreM222/copilot-lualine' }
 }
