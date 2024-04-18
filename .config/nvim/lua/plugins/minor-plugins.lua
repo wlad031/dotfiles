35739ce0280feaabe9fc0c057b7a74817f12f4f0
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-04-18 13:12:04 +0200

    minor nvim udpates

--- .config/nvim/lua/plugins/minor-plugins.lua
+++ .config/nvim/lua/plugins/minor-plugins.lua
@@ -17,10 +17,23 @@ local mini_starter = {
   end
 }
 
+local better_escape = {
+  "max397574/better-escape.nvim",
+  config = function()
+    require("better_escape").setup({
+      mapping = { "jk", "jj" }, -- a table with mappings to use
+      timeout = vim.o.timeoutlen,
+      clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+      keys = "<Esc>",           -- keys used for escaping, if it is a function will use the result everytime
+    })
+  end,
+}
+
 local plugins = {
   number_toggle,
   todo_comments,
-  mini_starter
+  mini_starter,
+  better_escape
 }
 
 return plugins
--- .config/nvim/lua/plugins/visuals.lua
+++ .config/nvim/lua/plugins/visuals.lua
@@ -12,32 +12,42 @@ local catppuccin = {
   end
 }
 
-local function lualine_copilot()
-  return {
-    'copilot',
-    symbols = {
-      status = {
-        hl = {
-          enabled = "#50FA7B",
-          sleep = "#AEB7D0",
-          disabled = "#6272A4",
-          warning = "#FFB86C",
-          unknown = "#FF5555"
-        }
-      },
-      spinners = require("copilot-lualine.spinners").dots,
-      spinner_color = "#6272A4"
-    },
-    show_colors = true,
-    show_loading = true
-  }
-end
-
 local lualine = {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons', opt = false },
   config = function()
     local lualine = require("lualine")
+
+    local function lualine_copilot()
+      return {
+        'copilot',
+        symbols = {
+          status = {
+            hl = {
+              enabled = "#50FA7B",
+              sleep = "#AEB7D0",
+              disabled = "#6272A4",
+              warning = "#FFB86C",
+              unknown = "#FF5555"
+            }
+          },
+          spinners = require("copilot-lualine.spinners").dots,
+          spinner_color = "#6272A4"
+        },
+        show_colors = true,
+        show_loading = true
+      }
+    end
+
+    local function escape_status()
+      local ok, m = pcall(require, 'better_escape')
+      if ok and m.waiting then
+        return '✺'
+      else
+        return ''
+      end
+    end
+
     lualine.setup({
       options = {
         theme = 'auto',
@@ -72,7 +82,7 @@ local plugins = {
   zenmode,
   catppuccin,
   lualine,
-  copilot_lualine,
+  --copilot_lualine,
 }
 
 return plugins
