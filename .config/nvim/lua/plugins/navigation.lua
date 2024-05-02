ef5933eb38d1645635c331e95e13b2f976d2f4be
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-05-02 16:54:49 +0200

    [autocommit] 2024-05-02T16:54:49+0200

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -16,9 +16,33 @@ local harpoon = {
   end
 }
 
+-- https://github.com/tris203/precognition.nvim
+-- 💭👀precognition.nvim - Precognition uses virtual text and gutter signs to show available motions.
+local precognition = {
+  "tris203/precognition.nvim",
+  config = {
+    -- startVisible = true,
+    -- hints = {
+    --     ["^"] = { text = "^", prio = 1 },
+    --     ["$"] = { text = "$", prio = 1 },
+    --     ["w"] = { text = "w", prio = 10 },
+    --     ["b"] = { text = "b", prio = 10 },
+    --     ["e"] = { text = "e", prio = 10 },
+    -- },
+    -- gutterHints = {
+    --     --prio is not currentlt used for gutter hints
+    --     ["G"] = { text = "G", prio = 1 },
+    --     ["gg"] = { text = "gg", prio = 1 },
+    --     ["{"] = { text = "{", prio = 1 },
+    --     ["}"] = { text = "}", prio = 1 },
+    -- },
+  },
+}
+
 local plugins = {
   flash,
   harpoon,
+  --precognition
 }
 
 return plugins
