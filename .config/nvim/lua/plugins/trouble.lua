b556ed0b0152320dd64cbe00259ccebb8861383c
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-06-24 18:41:37 +0200

    trouble v3 is now in main

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -3,7 +3,6 @@ require "user.keymaps"
 return {
   {
     "folke/trouble.nvim",
-    branch = "dev",
     keys = GetTroubleMapping(),
     opts = {}, -- for default options, refer to the configuration section for custom setup.
   }
