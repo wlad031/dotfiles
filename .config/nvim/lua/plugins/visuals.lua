0b9d88a8dc336de456c6634980f2a2b2d2b0c9aa
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2024-09-25 12:52:48 +0200

    [autocommit] 2024-09-25T12:52:48+0200

--- .config/nvim/lua/plugins/visuals.lua
+++ .config/nvim/lua/plugins/visuals.lua
@@ -62,9 +62,115 @@ local mini_animate = {
   end
 }
 
+local vim_illuminate = {
+  "RRethy/vim-illuminate",
+  config = function()
+    require("illuminate").configure({
+      -- providers: provider used to get references in the buffer, ordered by priority
+      providers = {
+        'lsp',
+        'treesitter',
+        'regex',
+      },
+      -- delay: delay in milliseconds
+      delay = 100,
+      -- filetype_overrides: filetype specific overrides.
+      -- The keys are strings to represent the filetype while the values are tables that
+      -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+      filetype_overrides = {},
+      -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+      filetypes_denylist = {
+        'dirbuf',
+        'dirvish',
+        'fugitive',
+      },
+      -- filetypes_allowlist: filetypes to illuminate, this is overridden by filetypes_denylist
+      -- You must set filetypes_denylist = {} to override the defaults to allow filetypes_allowlist to take effect
+      filetypes_allowlist = {},
+      -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+      -- See `:help mode()` for possible values
+      modes_denylist = {},
+      -- modes_allowlist: modes to illuminate, this is overridden by modes_denylist
+      -- See `:help mode()` for possible values
+      modes_allowlist = {},
+      -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+      -- Only applies to the 'regex' provider
+      -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+      providers_regex_syntax_denylist = {},
+      -- providers_regex_syntax_allowlist: syntax to illuminate, this is overridden by providers_regex_syntax_denylist
+      -- Only applies to the 'regex' provider
+      -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+      providers_regex_syntax_allowlist = {},
+      -- under_cursor: whether or not to illuminate under the cursor
+      under_cursor = true,
+      -- large_file_cutoff: number of lines at which to use large_file_config
+      -- The `under_cursor` option is disabled when this cutoff is hit
+      large_file_cutoff = nil,
+      -- large_file_config: config to use for large files (based on large_file_cutoff).
+      -- Supports the same keys passed to .configure
+      -- If nil, vim-illuminate will be disabled for large files.
+      large_file_overrides = nil,
+      -- min_count_to_highlight: minimum number of matches required to perform highlighting
+      min_count_to_highlight = 1,
+      -- should_enable: a callback that overrides all other settings to
+      -- enable/disable illumination. This will be called a lot so don't do
+      -- anything expensive in it.
+      should_enable = function(bufnr) return true end,
+      -- case_insensitive_regex: sets regex case sensitivity
+      case_insensitive_regex = false,
+    })
+  end
+}
+
+local treesitter_context = {
+  "nvim-treesitter/nvim-treesitter-context",
+  config = function()
+    require("treesitter-context").setup({
+      enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20,     -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    })
+  end
+}
+
+local neoscroll = {
+  "karb94/neoscroll.nvim",
+  config = function()
+    require('neoscroll').setup({
+      mappings = { -- Keys to be mapped to their corresponding default scrolling animation
+        '<C-u>', '<C-d>',
+        '<C-b>', '<C-f>',
+        '<C-y>', '<C-e>',
+        'zt', 'zz', 'zb',
+      },
+      hide_cursor = true,      -- Hide cursor while scrolling
+      stop_eof = true,         -- Stop at <EOF> when scrolling downwards
+      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      easing = 'linear',       -- Default easing function
+      pre_hook = nil,          -- Function to run before the scrolling animation starts
+      post_hook = nil,         -- Function to run after the scrolling animation ends
+      performance_mode = false, -- Disable "Performance Mode" on all buffers.
+      ignored_events = {       -- Events ignored while scrolling
+        'WinScrolled', 'CursorMoved'
+      },
+    })
+  end
+}
+
 return {
   -- tint,
   -- noice, -- it is fucking causing that cursor blinks when there is message
   -- mini_animate
+  vim_illuminate,
+  treesitter_context,
 }
-
