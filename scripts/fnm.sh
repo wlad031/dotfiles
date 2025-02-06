63ac842d2f8adeef6300dbd4efa7a59067855046
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2025-02-06 13:24:39 +0100

    [autocommit] 2025-02-06T13:24:39+0100

--- scripts/fnm.sh
+++ scripts/fnm.sh
@@ -1,6 +1,24 @@
+FNM_PATH="$HOME/.local/share/fnm"
+FNM_COMMAND_INSTALLED=false
+FNM_DIRECTORY_INSTALLED=false
+
 if ! command -v fnm &> /dev/null; then
-  export FNM_INSTALLED=false
+  FNM_COMMAND_INSTALLED=false
 else
+  FNM_COMMAND_INSTALLED=true
+fi
+
+if [[ "$FNM_COMMAND_INSTALLED" = false ]]; then
+  if [[ -d "$FNM_PATH" ]]; then
+    FNM_DIRECTORY_INSTALLED=true
+  else
+    FNM_DIRECTORY_INSTALLED=false
+    export FNM_INSTALLED=false
+  fi
+fi
+
+if [[ "$FNM_COMMAND_INSTALLED" = false && "$FNM_DIRECTORY_INSTALLED" = true ]]; then
+  export PATH="$FNM_PATH:$PATH"
   export FNM_INSTALLED=true
 fi
 
@@ -9,6 +27,6 @@ fnm_setup() {
     log_error "fnm (Node.js version manager) is not installed"
     return
   fi
-    
+
   eval "$(fnm env --use-on-cd)"
 }
