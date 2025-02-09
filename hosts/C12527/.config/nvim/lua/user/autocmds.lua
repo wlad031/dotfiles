62f5d8e03f199369fead435778f0ddbe97669131
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2025-02-09 16:08:58 +0100

    [autocommit] 2025-02-09T16:08:58+0100

--- hosts/C12527/.config/nvim/lua/user/autocmds.lua
+++ hosts/C12527/.config/nvim/lua/user/autocmds.lua
@@ -1,13 +1,13 @@
 -- Restore cursor to file position in previous editing session
-vim.api.nvim_create_autocmd("BufReadPost", {
-  callback = function(args)
-    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
-    local line_count = vim.api.nvim_buf_line_count(args.buf)
-    if mark[1] > 0 and mark[1] <= line_count then
-      vim.cmd('normal! g`"zz')
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+--   callback = function(args)
+--     local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+--     local line_count = vim.api.nvim_buf_line_count(args.buf)
+--     if mark[1] > 0 and mark[1] <= line_count then
+--       vim.cmd('normal! g`"zz')
+--     end
+--   end,
+-- })
 
 -- removes trailing whitespace on save
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -19,14 +19,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 -- highlights yanked text
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "IncSearch",
-      timeout = 40,
-    })
-  end,
-})
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+--   callback = function()
+--     vim.highlight.on_yank({
+--       higroup = "IncSearch",
+--       timeout = 40,
+--     })
+--   end,
+-- })
 
 -- Dim inactive windows
 
@@ -44,98 +44,98 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- })
 
 -- When entering a window, restore all highlight groups to original
-vim.api.nvim_create_autocmd({ "WinEnter" }, {
-  callback = function()
-    vim.wo.winhighlight = ""
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "WinEnter" }, {
+--   callback = function()
+--     vim.wo.winhighlight = ""
+--   end,
+-- })
 
 -- Keep the cursor position when yanking
-local cursorPreYank
-
-vim.keymap.set({ "n", "x" }, "y", function()
-  cursorPreYank = vim.api.nvim_win_get_cursor(0)
-  return "y"
-end, { expr = true })
-
-vim.keymap.set("n", "Y", function()
-  cursorPreYank = vim.api.nvim_win_get_cursor(0)
-  return "y$"
-end, { expr = true })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    if vim.v.event.operator == "y" and cursorPreYank then
-      vim.api.nvim_win_set_cursor(0, cursorPreYank)
-    end
-  end,
-})
+-- local cursorPreYank
+--
+-- vim.keymap.set({ "n", "x" }, "y", function()
+--   cursorPreYank = vim.api.nvim_win_get_cursor(0)
+--   return "y"
+-- end, { expr = true })
+--
+-- vim.keymap.set("n", "Y", function()
+--   cursorPreYank = vim.api.nvim_win_get_cursor(0)
+--   return "y$"
+-- end, { expr = true })
+--
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+--   callback = function()
+--     if vim.v.event.operator == "y" and cursorPreYank then
+--       vim.api.nvim_win_set_cursor(0, cursorPreYank)
+--     end
+--   end,
+-- })
 
 -- Show cursorline only on active windows
-vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-  callback = function()
-    if vim.w.auto_cursorline then
-      vim.wo.cursorline = true
-      vim.w.auto_cursorline = false
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
-  callback = function()
-    if vim.wo.cursorline then
-      vim.w.auto_cursorline = true
-      vim.wo.cursorline = false
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+--   callback = function()
+--     if vim.w.auto_cursorline then
+--       vim.wo.cursorline = true
+--       vim.w.auto_cursorline = false
+--     end
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+--   callback = function()
+--     if vim.wo.cursorline then
+--       vim.w.auto_cursorline = true
+--       vim.wo.cursorline = false
+--     end
+--   end,
+-- })
 
 
 -- Auto resize splits when the terminal's window is resized
-vim.api.nvim_create_autocmd({ "VimResized" }, {
-  group = vim.api.nvim_create_augroup("EqualizeSplits", {}),
-  callback = function()
-    local current_tab = vim.api.nvim_get_current_tabpage()
-    vim.cmd("tabdo wincmd =")
-    vim.api.nvim_set_current_tabpage(current_tab)
-  end,
-  desc = "Resize splits with terminal window",
-})
+-- vim.api.nvim_create_autocmd({ "VimResized" }, {
+--   group = vim.api.nvim_create_augroup("EqualizeSplits", {}),
+--   callback = function()
+--     local current_tab = vim.api.nvim_get_current_tabpage()
+--     vim.cmd("tabdo wincmd =")
+--     vim.api.nvim_set_current_tabpage(current_tab)
+--   end,
+--   desc = "Resize splits with terminal window",
+-- })
 
 -- Close on "q"
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "help",
-    "startuptime",
-    "qf",
-    "lspinfo",
-    "man",
-    "checkhealth",
-    "neotest-output-panel",
-    "neotest-summary",
-    "lazy",
-  },
-  command = [[
-          nnoremap <buffer><silent> q :close<CR>
-          nnoremap <buffer><silent> <ESC> :close<CR>
-          set nobuflisted
-      ]],
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = {
+--     "help",
+--     "startuptime",
+--     "qf",
+--     "lspinfo",
+--     "man",
+--     "checkhealth",
+--     "neotest-output-panel",
+--     "neotest-summary",
+--     "lazy",
+--   },
+--   command = [[
+--           nnoremap <buffer><silent> q :close<CR>
+--           nnoremap <buffer><silent> <ESC> :close<CR>
+--           set nobuflisted
+--       ]],
+-- })
 
-vim.api.nvim_create_autocmd('CmdlineEnter', {
-  group = vim.api.nvim_create_augroup(
-    'gmr_cmdheight_1_on_cmdlineenter',
-    { clear = true }
-  ),
-  desc = 'Don\'t hide the status line when typing a command',
-  command = ':set cmdheight=1',
-})
-
-vim.api.nvim_create_autocmd('CmdlineLeave', {
-  group = vim.api.nvim_create_augroup(
-    'gmr_cmdheight_0_on_cmdlineleave',
-    { clear = true }
-  ),
-  desc = 'Hide cmdline when not typing a command',
-  command = ':set cmdheight=0',
-})
+-- vim.api.nvim_create_autocmd('CmdlineEnter', {
+--   group = vim.api.nvim_create_augroup(
+--     'gmr_cmdheight_1_on_cmdlineenter',
+--     { clear = true }
+--   ),
+--   desc = 'Don\'t hide the status line when typing a command',
+--   command = ':set cmdheight=1',
+-- })
+--
+-- vim.api.nvim_create_autocmd('CmdlineLeave', {
+--   group = vim.api.nvim_create_augroup(
+--     'gmr_cmdheight_0_on_cmdlineleave',
+--     { clear = true }
+--   ),
+--   desc = 'Hide cmdline when not typing a command',
+--   command = ':set cmdheight=0',
+-- })
