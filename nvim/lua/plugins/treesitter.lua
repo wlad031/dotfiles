ecc30266f7888611a789118c28a47e8fe9231952
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2026-08-30 13:58:28 +0200

    nvim: update treesitter setup

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,37 @@
+local parsers = {
+  "lua",
+  "regex",
+  "html",
+  "java",
+  "scala",
+  "clojure",
+  "python",
+  "bash",
+  "markdown",
+  "xml",
+  "json",
+  "yaml",
+  "markdown_inline",
+  "ledger",
+  "jinja",
+  "jinja_inline",
+  "latex",
+  "terraform",
+  "vim",
+  "vimdoc",
+  "query",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false,  -- Keep false to ensure loading for Neo-tree and other plugins
-    build = ":TSUpdate",
+    branch = "main",
+    lazy = false, -- nvim-treesitter does not support lazy-loading
+    build = function()
+      require("nvim-treesitter").install(parsers):wait(300000)
+    end,
     config = function()
-      -- Add jinja filetype mappings
+      -- Add jinja filetype mappings.
       vim.filetype.add({
         extension = {
           j2 = "jinja",
@@ -12,46 +39,22 @@ return {
           jinja2 = "jinja",
         },
       })
-      -- Register jinja2 parser
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.jinja2 = {
-        install_info = {
-          url = "https://github.com/dbt-labs/tree-sitter-jinja2",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "jinja",
-      }
-      -- Setup treesitter
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua",
-          "regex",
-          "html",
-          "java",
-          "scala",
-          "clojure",
-          "python",
-          "bash",
-          "markdown",
-          "xml",
-          "json",
-          "yaml",
-          "markdown_inline",
-          "ledger",
-          "jinja2",
-          "latex",
-          "terraform",
-          "vim",
-          "vimdoc",
-          "query",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
+
+      require("nvim-treesitter").setup({
+        install_dir = vim.fn.stdpath("data") .. "/site",
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = parsers,
+        callback = function(args)
+          -- Some parser names are not filetypes (for example markdown_inline,
+          -- query). pcall keeps those buffers from producing startup errors.
+          pcall(vim.treesitter.start, args.buf)
+        end,
       })
     end,
   },
--- {
+  -- {
   --   "nvim-treesitter/nvim-treesitter-textobjects",
   --   dependencies = { "nvim-treesitter/nvim-treesitter" },
   -- },
@@ -67,18 +70,18 @@ return {
   --       min_window_height = 0,  -- Minimum editor window height to enable context. Values <= 0 mean no limit.
   --       line_numbers = true,
   --       multiline_threshold = 20, -- Maximum number of lines to show for a single context
-  --       trim_scope = 'outer',   -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  --       mode = 'cursor',        -- Line used to calculate context. Choices: 'cursor', 'topline'
-  --       -- Separator between context and content. Should be a single character string, like '-'.
+  --       trim_scope = "outer",   -- Which context lines to discard if `max_lines` is exceeded. Choices: "inner", "outer"
+  --       mode = "cursor",        -- Line used to calculate context. Choices: "cursor", "topline"
+  --       -- Separator between context and content. Should be a single character string, like "-".
   --       -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
   --       separator = nil,
   --       zindex = 20,   -- The Z-index of the context window
   --       on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
   --     })
-  --     vim.keymap.set('n', '<leader>vct', treesitter_context.toggle)
-  --     -- require('which-key').register({
-  --     --   { '', group = 'Visuals' },
-  --     --   { '', group = 'Context' },
+  --     vim.keymap.set("n", "<leader>vct", treesitter_context.toggle)
+  --     -- require("which-key").register({
+  --     --   { "", group = "Visuals" },
+  --     --   { "", group = "Context" },
   --     -- })
   --   end
   -- }
